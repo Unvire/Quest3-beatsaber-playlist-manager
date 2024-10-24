5368b2bd9fcf878dc11de948ba29c3bd14009a64
Unvire <krzysztof.balcerzak6@gmail.com>
2024-10-24 15:12:54 +0200

    adds author for the repr test

--- tests/test_beatSaberMap.py
+++ tests/test_beatSaberMap.py
@@ -4,5 +4,8 @@ import beatSaberMap
 def test_initAndRepr():
     song = beatSaberMap.BeatSaberMap('1234')
     song._setNameAndHash('test song', 'BHVASJDVHasd')
-    expected =  "Song: {'Name': 'test song', 'ID': '1234', 'Hash': 'BHVASJDVHasd'}"
+    song._setAuthorAndUrls('author', 'a', 'b')
+    expected =  "Song: {'Name': 'test song', 'ID': '1234', 'Hash': 'BHVASJDVHasd', 'Author': 'author'}"
     assert song.__repr__() == expected
+
+    
