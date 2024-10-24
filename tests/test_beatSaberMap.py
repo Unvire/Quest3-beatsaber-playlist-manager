b4989cfc80563c9f62d41765a8e2ffbefb39c741
Unvire <krzysztof.balcerzak6@gmail.com>
2024-10-24 15:21:08 +0200

    adds test for generating playlist song entry

--- tests/test_beatSaberMap.py
+++ tests/test_beatSaberMap.py
@@ -8,4 +8,13 @@ def test_initAndRepr():
     expected =  "Song: {'Name': 'test song', 'ID': '1234', 'Hash': 'BHVASJDVHasd', 'Author': 'author'}"
     assert song.__repr__() == expected
 
-    
+def test_generateDictForPlaylist():
+    song = beatSaberMap.BeatSaberMap('1234')
+    song.hash = 'eae95f9d7700b2e9b724ffe5ad23b4541f701181'
+    song.name = 'test song'
+    expected = {
+        'key': '1234',
+        'hash': 'eae95f9d7700b2e9b724ffe5ad23b4541f701181',
+        'songName': 'test song'
+    }
+    assert song.generateDictForPlaylist() == expected
