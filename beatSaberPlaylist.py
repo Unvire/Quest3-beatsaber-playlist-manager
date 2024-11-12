ccb983f0b56e9bb4720b77c8f13906c97472859e
Unvire <125924929+Unvire@users.noreply.github.com>
2024-11-12 19:36:33 +0100

    Adds playlist class template

---
+++ beatSaberPlaylist.py
@@ -0,0 +1,31 @@
+class BeatSaberPlaylist:
+    def __init__(self):
+        pass
+
+    def loadFromFile(self):
+        pass
+
+    def saveToFile(self):
+        pass
+
+    def addSong(self):
+        pass
+
+    def removeSong(self):
+        pass
+
+    def select(self):
+        pass
+
+    def unselect(self):
+        pass
+
+    def moveSelectedItemsUp(self):
+        pass
+
+    def moveSelectedItemsDown(self):
+        pass
+
+
+if __name__ == '__main__':
+    a = BeatSaberPlaylist()
