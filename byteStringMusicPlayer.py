cd2fad39822a79683af37cf44ae006d2bf9b6980
Unvire <125924929+Unvire@users.noreply.github.com>
2024-11-26 20:55:48 +0100

    splits playing into loading and playing. fixes error with calling player 2 times. fixes music overlapping

--- byteStringMusicPlayer.py
+++ byteStringMusicPlayer.py
@@ -5,22 +5,30 @@ class ByteStringMusicPlayer():
     CHUNK_SIZE_BYTES = 1024
 
     def __init__(self):
-        self.player = pyaudio.PyAudio()
-        self.stream = None                
+        self.player = None
+        self.stream = None  
         self.isPlaying = False 
-        self.thread = None       
+        self.thread = None
+        self.unconvertedByteString = ''
+        self.fileFormat = ''
 
-    def playFromUrl(self, url:str):
+    def loadMusicFromUrl(self, url:str):
         response = requests.get(url)
-        byteString = response.content
-
-        fileFormat = url.split('.')[-1]
-        audio = pydub.AudioSegment.from_file(io.BytesIO(byteString), format=fileFormat)
+        self.unconvertedByteString = response.content
+        self.fileFormat = url.split('.')[-1]
+        self.thread = None
+    
+    def play(self):
+        if not self.unconvertedByteString or self.thread or self.isPlaying:
+            return
+        
+        self.player = pyaudio.PyAudio()
+        audio = pydub.AudioSegment.from_file(io.BytesIO(self.unconvertedByteString), 
+                                             format=self.fileFormat)
         self.stream = self.player.open(format=self.player.get_format_from_width(audio.sample_width),
                                         channels=audio.channels,
                                         rate=audio.frame_rate,
                                         output=True)
-        
         self.thread = threading.Thread(target=self._playAudioInChunks, args=(audio,))                
         self.isPlaying = True
         self.thread.start()
@@ -36,9 +44,9 @@ class ByteStringMusicPlayer():
         self.player.terminate()
     
     def _playAudioInChunks(self, audio:pydub.AudioSegment):
-        audio_data = io.BytesIO(audio.raw_data)
+        audioData = io.BytesIO(audio.raw_data)
         while self.isPlaying:
-            chunk = audio_data.read(ByteStringMusicPlayer.CHUNK_SIZE_BYTES)
+            chunk = audioData.read(ByteStringMusicPlayer.CHUNK_SIZE_BYTES)
             if not chunk:
                 break
             self.stream.write(chunk)    
@@ -50,7 +58,8 @@ if __name__ == '__main__':
     this class requires FFMPEG installed
     '''
     a = ByteStringMusicPlayer()
-    a.playFromUrl('https://eu.cdn.beatsaver.com/b8c98ffc598703aadb4a3cb921d2830d270b57a5.mp3')
+    a.loadMusicFromUrl('https://eu.cdn.beatsaver.com/b8c98ffc598703aadb4a3cb921d2830d270b57a5.mp3')
+    a.play()
     input('Press any key to stop...')
     a.stop()
 
