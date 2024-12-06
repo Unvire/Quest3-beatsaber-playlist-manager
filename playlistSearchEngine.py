27432978315c4b60ebab932056ebf744d6c2391a
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-06 14:56:45 +0100

    searching through longstring and init keywords search

--- playlistSearchEngine.py
+++ playlistSearchEngine.py
@@ -25,18 +25,24 @@ class SearchEngine:
             '__mods': mapInstance.getRequiredMods(),
             '__nps': mapInstance.getNpsRange(),
             '__njs': mapInstance.getNjsRange(),
-            '__uploaded': mapInstance.uploaded,
+            #'__uploaded': mapInstance.uploaded, #
             '__stars': mapInstance.getStarsRange(),
             '__state': mapInstance.rankedState,
         }
         return cacheDict
     
     def filterMaps(self, searchRequestString:str) -> list[int]:
+        if not searchRequestString:
+            return [i for i in range(len(self.cache))]
+        
         words = searchRequestString.split(' ')
-        keywordsInRequest = self.keywords & set(words)
-        regexSearchWords = set(words) - keywordsInRequest
+        regexSearchWords = words[0] if words[0].startswith('__') else None
+        keywords = self._findKeywords(words)
 
-        matchingLongStringIndexes = self._checkLongstrings(regexSearchWords)
+        if regexSearchWords:
+            matchingLongStringIndexes = self._checkLongstrings(regexSearchWords)
+        if keywords:
+            matchingKeywordsIndexes = self._checkKeywords(keywords)
 
     def _checkLongstrings(self, regexSearchWords:str) -> list[int]:
         result = []
@@ -49,6 +55,41 @@ class SearchEngine:
             if isMatch:
                 result.append(i)
         return i
+    
+    def _findKeywords(self, words:list[str]) -> list[list[str, str]]:
+        keywords = []
+        for word in words:
+            subWords = word.split('=')
+            if len(subWords) != 2:
+                continue
 
-    def _checkKeywords(self, keywords:list[str]) -> list[int]:
-        pass
+            if not subWords[0] in self.keywords:
+                continue
+            
+            keyword, criteria = subWords
+            keywords.append([keyword, criteria])
+        return keywords
+
+    def _checkKeywords(self, keywords:list[list[str, str]]) -> list[int]:
+        rangeKeywords = ['__length', '__bpm',  '__nps', '__njs', '__stars']
+        stringKeywords = ['__mods', '__state', '__stars']
+        result = []
+        for keyword, criteria in keywords:
+            for i, cacheDict in enumerate(self.cache):
+                if keyword in rangeKeywords:
+                    isInRange = self._checkIfInRange(self, criteria, cacheDict[keyword])
+                    if isInRange:
+                        result.append(i)
+                        
+                if keyword in stringKeywords:
+                    pass
+    
+    def _checkIfInRange(self, criteria:str, value:float|int) -> bool:
+        rangePattern = '\[(-?\d*(?:\.\d+)?)?\s*;\s*(-?\d*(?:\.\d+)?)?]' # checks if criteria is 2 numbers [1.2;2.3], 1 number [;1] or [1;] or [;]
+        if not re.match(rangePattern, criteria):
+            return True
+        
+        minValStr, maxValStr = criteria[1:-1].split(';')
+        minVal = float(minValStr) if minValStr else float('-inf')
+        maxVal = float(maxValStr) if maxValStr else float('inf')
+        return minVal <= float(value) <= maxVal
