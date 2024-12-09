89cff8bc3be1326b18642653a150dc63d7c25d41
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-09 13:32:22 +0100

    moves test files to folder inside test folder

---
+++ tests/test_playlistSearchEngine.py
@@ -0,0 +1,16 @@
+import pytest
+import playlistSearchEngine
+import beatSaberMap, beatSaberMapLevel
+
+@pytest.fixture
+def mockPlaylist():
+    song1 = beatSaberMap.BeatSaberMap(40750)
+    song2 =
+    song3 =
+    song4 =
+    song5 =
+    song6 =
+    song7 =
+    song8 =
+    song9 =
+    song10 =
