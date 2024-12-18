2800d39622ef39b9ba571e36e3f0071325f6bd9e
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-18 15:47:01 +0100

    Adds checkbox handling

--- filterMapsDialog.py
+++ filterMapsDialog.py
@@ -1,5 +1,5 @@
 import sys, os
-from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit, QCheckBox
 from PyQt5 import uic
 
 from beatSaberMap import BeatSaberMap
@@ -11,6 +11,9 @@ class FilterMapsDialog(QDialog):
         uiFilePath = os.path.join(os.getcwd(), 'ui', 'searchMapsDialog.ui')
         uic.loadUi(uiFilePath, self)
 
+        self.rankedStateSelectionSet = set()
+        self.modsSelectionSet = set()
+
         self.lengthEdit.editingFinished.connect(lambda: self._validateEdit(self.lengthEdit, 'numeric'))
         self.bpmEdit.editingFinished.connect(lambda: self._validateEdit(self.bpmEdit, 'numeric'))
         self.npsEdit.editingFinished.connect(lambda: self._validateEdit(self.npsEdit, 'numeric'))
@@ -18,11 +21,41 @@ class FilterMapsDialog(QDialog):
         self.starsEdit.editingFinished.connect(lambda: self._validateEdit(self.starsEdit, 'numericOrStr'))
         self.uploadedEdit.editingFinished.connect(lambda: self._validateEdit(self.uploadedEdit, 'date'))
 
+        self.graveyardCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Graveyard'))
+        self.qualifiedCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Qualified'))
+        self.rankedCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Ranked'))
+        
+        self.noModsCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'No mods'))
+        self.chromaCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'Chroma'))
+        self.neCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'Noodle Extensions'))
+        self.meCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'Mapping Extensions'))
+        self.cinemaCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'Cinema'))
+
         self.okButton.clicked.connect(self.accept)
         self.cancelButton.clicked.connect(self.reject)
     
     def _validateEdit(self, editHandle:QLineEdit, editType:str):
         print(editHandle.text(), editType)
+    
+    def _rankedStateCheckboxToggled(self, state:bool, value:str):
+        if state:
+            self.rankedStateSelectionSet.add(value)
+        else:
+            self.rankedStateSelectionSet.remove(value)
+        
+        selectedStates = ', '.join(sorted(list(self.rankedStateSelectionSet)))
+        selectedStates = 'Any' if not selectedStates else selectedStates
+        self.rankedStateLabel.setText(f'Ranked state: {selectedStates}')
+    
+    def _modsCheckboxToggled(self, state:bool, value:str):
+        if state:
+            self.modsSelectionSet.add(value)
+        else:
+            self.modsSelectionSet.remove(value)
+        
+        selectedStates = ', '.join(sorted(list(self.modsSelectionSet)))
+        selectedStates = 'Any' if not selectedStates else selectedStates
+        self.modsLabel.setText(f'Mods: {selectedStates}')
 
 
 if __name__ == '__main__':
