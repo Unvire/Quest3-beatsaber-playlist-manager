ace7e911a6c44024741c636cc241965311c59199
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-18 15:23:42 +0100

    Adds Edit editing finished event

--- filterMapsDialog.py
+++ filterMapsDialog.py
@@ -1,5 +1,5 @@
 import sys, os
-from PyQt5.QtWidgets import QApplication, QDialog, QVBoxLayout, QLabel
+from PyQt5.QtWidgets import QApplication, QDialog, QLineEdit
 from PyQt5 import uic
 
 from beatSaberMap import BeatSaberMap
@@ -11,8 +11,18 @@ class FilterMapsDialog(QDialog):
         uiFilePath = os.path.join(os.getcwd(), 'ui', 'searchMapsDialog.ui')
         uic.loadUi(uiFilePath, self)
 
+        self.lengthEdit.editingFinished.connect(lambda: self._validateEdit(self.lengthEdit, 'numeric'))
+        self.bpmEdit.editingFinished.connect(lambda: self._validateEdit(self.bpmEdit, 'numeric'))
+        self.npsEdit.editingFinished.connect(lambda: self._validateEdit(self.npsEdit, 'numeric'))
+        self.njsEdit.editingFinished.connect(lambda: self._validateEdit(self.njsEdit, 'numeric'))
+        self.starsEdit.editingFinished.connect(lambda: self._validateEdit(self.starsEdit, 'numericOrStr'))
+        self.uploadedEdit.editingFinished.connect(lambda: self._validateEdit(self.uploadedEdit, 'date'))
+
         self.okButton.clicked.connect(self.accept)
         self.cancelButton.clicked.connect(self.reject)
+    
+    def _validateEdit(self, editHandle:QLineEdit, editType:str):
+        print(editHandle.text(), editType)
 
 
 if __name__ == '__main__':
