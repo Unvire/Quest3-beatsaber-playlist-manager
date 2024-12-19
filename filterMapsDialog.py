ff3b9597cbf93f3e6b987d2c53ad726f0cec897a
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-19 14:45:58 +0100

    adds mothods that return maps not to be hidden

--- filterMapsDialog.py
+++ filterMapsDialog.py
@@ -3,29 +3,22 @@ from PyQt5 import uic
 
 import sys, os, re
 from beatSaberMap import BeatSaberMap
+from beatSaberPlaylist import BeatSaberPlaylist
 
 
 class FilterMapsDialog(QDialog):
     FLOAT_PATTERN = '(-?\d*(?:\.\d+)?)?'
     RANGE_PATTERN = f'^\[{FLOAT_PATTERN};{FLOAT_PATTERN}]$'
 
-    def __init__(self, mapsCacheList:list[dict]):
+    def __init__(self, playlist:BeatSaberPlaylist):
         super().__init__()
         uiFilePath = os.path.join(os.getcwd(), 'ui', 'searchMapsDialog.ui')
         uic.loadUi(uiFilePath, self)
 
+        self.playlist = playlist
+
         self.rankedStateSelectionSet = set()
         self.modsSelectionSet = set()
-
-        self.lengthEdit.editingFinished.connect(lambda: self._validateEdit(self.lengthEdit, 'numeric'))
-        self.bpmEdit.editingFinished.connect(lambda: self._validateEdit(self.bpmEdit, 'numeric'))
-        self.npsEdit.editingFinished.connect(lambda: self._validateEdit(self.npsEdit, 'numeric'))
-        self.njsEdit.editingFinished.connect(lambda: self._validateEdit(self.njsEdit, 'numeric'))
-        self.starsEdit.editingFinished.connect(lambda: self._validateEdit(self.starsEdit, 'numericOrStr'))
-
-        self.graveyardCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Graveyard'))
-        self.qualifiedCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Qualified'))
-        self.rankedCheckbox.toggled.connect(lambda state: self._rankedStateCheckboxToggled(state, 'Ranked'))
         
         self.noModsCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'No mods'))
         self.chromaCheckbox.toggled.connect(lambda state: self._modsCheckboxToggled(state, 'Chroma'))
@@ -36,8 +29,60 @@ class FilterMapsDialog(QDialog):
         self.okButton.clicked.connect(self.accept)
         self.cancelButton.clicked.connect(self.reject)
     
-    def _validateEdit(self, editHandle:QLineEdit, editType:str):
-        print(editHandle.text(), editType)
+    def getData(self) -> list[int]:
+        longStringPattern = self.parametersEdit.text()
+        requiredLength = self._extractRangeValuesFromString(self.lengthEdit.text())
+        requiredBpm = self._extractRangeValuesFromString(self.bpmEdit.text())
+        requiredNps = self._extractRangeValuesFromString(self.npsEdit.text())
+        requiredNjs = self._extractRangeValuesFromString(self.njsEdit.text())
+        requiredStars = self._extractRangeValuesFromString(self.starsEdit.text())
+        requiredRankedStates = self.rankedStateSelectionSet
+        requiredMods = self.modsSelectionSet
+
+        return self._filterMaps(longStringPattern=longStringPattern, requiredLength=requiredLength, requiredBpm=requiredBpm, requiredNps=requiredNps,
+                                requiredNjs=requiredNjs, requiredStars=requiredStars, requiredRankedStates=requiredRankedStates, requiredMods=requiredMods)
+    
+    def _filterMaps(self, longStringPattern:str='', requiredLength:tuple[float, float]|str='', requiredBpm:tuple[float, float]|str='', 
+                    requiredNps:tuple[float, float]|str='', requiredNjs:tuple[float, float]|str='', requiredStars:tuple[float, float]|str='', 
+                    requiredRankedStates=list[str], requiredMods=list[str]) -> list[int]:
+        result = []
+        for i, song in enumerate(self.playlist):
+            criteriaMatched = []
+            cache = song.getCacheData()
+
+            criteriaMatched.append(bool(re.match(longStringPattern, cache['longString'])))
+            criteriaMatched.append(self._checkRangeOrStr(cache['length'], requiredLength))
+            criteriaMatched.append(self._checkRangeOrStr(cache['bpm'], requiredBpm))
+            criteriaMatched.append(self._checkRangeOrStr(cache['nps'], requiredNps))
+            criteriaMatched.append(self._checkRangeOrStr(cache['njs'], requiredNjs))            
+            criteriaMatched.append(self._checkRangeOrStr(cache['stars'], requiredStars))
+            criteriaMatched.append(not requiredRankedStates or cache['rankedState'] in requiredRankedStates)
+
+            requiredMods = set(requiredMods)
+            criteriaMatched.append(not requiredMods or bool(cache['mods'] & requiredMods))
+            if not all(criteriaMatched):
+                result.append(i)
+        return result
+    
+    def _checkRangeOrStr(self, cacheVal:str, requiredValue:str|tuple[float, float]) -> bool:
+        if isinstance(requiredValue, tuple):
+            requiredMinVal, requiredMaxVal = requiredValue
+            minVal, maxVal = cacheVal
+            return requiredMinVal <= minVal and requiredMaxVal >= maxVal
+        else:
+            return requiredValue == cacheVal
+    
+    def _extractRangeValuesFromString(self, keywordData:str) -> str | tuple[float, float]:
+        floatPattern = '(-?\d*(?:\.\d+)?)?' # checks for float 1.2, 0.2 .2 or int
+        rangePattern = f'^\[{floatPattern};{floatPattern}]$' # checks if criteria is 2 numbers [1.2;2.3], 1 number [;1] or [1;] or [;]
+
+        keywordData = keywordData.strip().replace(' ', '')        
+        if re.search(rangePattern, keywordData):
+            minVal, maxVal = keywordData[1:-1].split(';')
+            minVal = float(minVal) if minVal else float('-inf')
+            maxVal = float(maxVal) if maxVal else float('inf')
+            return minVal, maxVal
+        return keywordData
 
     def _rankedStateCheckboxToggled(self, state:bool, value:str):
         if state:
@@ -61,7 +106,7 @@ class FilterMapsDialog(QDialog):
 
 
 if __name__ == '__main__':
-    items = [{}, {}]
+    items = BeatSaberPlaylist()
     app = QApplication(sys.argv)
     window = FilterMapsDialog(items)
     window.show()
