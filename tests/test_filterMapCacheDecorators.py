2ebd8b8460a2651a99c4cf173145babe570db906
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-23 11:34:32 +0100

    Adds tests for CheckRangeOrString

--- tests/test_filterMapCacheDecorators.py
+++ tests/test_filterMapCacheDecorators.py
@@ -24,7 +24,17 @@ def test__checkRangeOrStr(inputCacheValue, inputRequiredValue, expected):
     assert filterMapCacheDecorators.CheckRangeOrString._checkRangeOrStr(None, inputCacheValue, inputRequiredValue) == expected
 
 @pytest.mark.parametrize('inputData, expected', [('chall', True), ('test', False), ('.....', True), ('(pop|hard|eeeasy)', True)])
-def test__CheckLongString(inputData, expected, mockBaseNode):
+def test_CheckLongString(inputData, expected, mockBaseNode):
     mockBaseNode = filterMapCacheDecorators.CheckLongString(mockBaseNode, 'longString', inputData)
     assert mockBaseNode.checkCriteria() == expected
 
+@pytest.mark.parametrize('inputData, expected', [('252', True), ('253', False), ('nasjidbasud', False), ((100, 300), True), ((100, 101), False), ((100, 252.99), False)])
+def test_CheckRangeOrString_oneValueCached(inputData, expected, mockBaseNode):
+    #'length': 252
+    mockBaseNode = filterMapCacheDecorators.CheckRangeOrString(mockBaseNode, 'length', inputData) == expected
+
+@pytest.mark.parametrize('inputData, expected', [((3, 5), True), ((3.5, 4.2), True), ((3.5, 4.101), True), ((3.5, 4.1), False), ((3.862, 10), True), 
+    ((1, 2), False), ((3, 10), False), ((1, 4), False), ((6, 10), False)])
+def test_CheckRangeOrString_RangeCached(inputData, expected, mockBaseNode):
+    #'nps': (3.862, 4.101)
+    mockBaseNode = filterMapCacheDecorators.CheckRangeOrString(mockBaseNode, 'nps', inputData) == expected
