d961061f62fc32646e710d6ca1b469829309dbc2
Unvire <krzysztof.balcerzak6@gmail.com>
2024-12-23 12:23:47 +0100

    Adds tests for criteria decorator chaining

--- tests/test_filterMapCacheDecorators.py
+++ tests/test_filterMapCacheDecorators.py
@@ -9,15 +9,15 @@ def mockBaseNode():
         'bpm': 147.0, 
         'mods': {'ne', 'chroma'}, 
         'nps': (3.862, 4.101), 
-        'njs': (16.0, 16.0), 
+        'njs': 16.0,
         'stars': '?', 
         'rankedState': 'Graveyard'
     }
     return filterMapCacheDecorators.BaseCacheNode(cache)
 
 @pytest.mark.parametrize('inputCacheValue, inputRequiredValue, expected', [
-    ((1, 3), (0, 3), False), ((1, 3), (2, 3), True), ((0, 4), (2, 3), True), ((2, 3), (2, 3), True), ((2, 3), (3, 2), True), ((2, 3), (0, 4), False), ((0, 2), (0, 3), False),
-    (2, (0, 3), True), (-2, (0, 3), False), (0, (0, 3), True), (3, (0, 3), True),
+    ((1, 3), (0, 3), False), ((1, 3), (2, 3), True), ((0, 4), (2, 3), True), ((2, 3), (2, 3), True), ((2, 3), (3, 2), True), 
+    ((2, 3), (0, 4), False), ((0, 2), (0, 3), False),(2, (0, 3), True), (-2, (0, 3), False), (0, (0, 3), True), (3, (0, 3), True),
     ('test', 'test', True), ('test', '', False), ('', '', True)])
 def test__checkRangeOrStr(inputCacheValue, inputRequiredValue, expected):
     assert filterMapCacheDecorators.CheckRangeOrString._checkRangeOrStr(None, inputCacheValue, inputRequiredValue) == expected
@@ -27,7 +27,8 @@ def test_CheckLongString(inputData, expected, mockBaseNode):
     mockBaseNode = filterMapCacheDecorators.CheckLongString(mockBaseNode, 'longString', inputData)
     assert mockBaseNode.checkCriteria() == expected
 
-@pytest.mark.parametrize('inputData, expected', [('252', True), ('253', False), ('nasjidbasud', False), ((100, 300), True), ((100, 101), False), ((100, 252.99), False)])
+@pytest.mark.parametrize('inputData, expected', [('252', True), ('253', False), ('nasjidbasud', False), 
+    ((100, 300), True), ((100, 101), False), ((100, 252.99), False)])
 def test_CheckRangeOrString_oneValueCached(inputData, expected, mockBaseNode):
     #'length': 252
     mockBaseNode = filterMapCacheDecorators.CheckRangeOrString(mockBaseNode, 'length', inputData) == expected
@@ -38,8 +39,31 @@ def test_CheckRangeOrString_RangeCached(inputData, expected, mockBaseNode):
     #'nps': (3.862, 4.101)
     mockBaseNode = filterMapCacheDecorators.CheckRangeOrString(mockBaseNode, 'nps', inputData) == expected
 
-@pytest.mark.parametrize('inputData, expected', [(['ne'], True), (['chroma'], True), (['ne', 'chroma'], True), ([], False), (['me'], False), (['me', 'chroma'], True)])
+@pytest.mark.parametrize('inputData, expected', [(['ne'], True), (['chroma'], True), (['ne', 'chroma'], True), 
+    ([], False), (['me'], False), (['me', 'chroma'], True)])
 def test_CheckLongString(inputData, expected, mockBaseNode):
     #'mods': {'ne', 'chroma'}
     mockBaseNode = filterMapCacheDecorators.CheckValueSet(mockBaseNode, 'mods', inputData)
     assert mockBaseNode.checkCriteria() == expected
+
+@pytest.mark.parametrize('inputDataList, expected', [
+        ([('longString', 'expert')], True),
+        ([('longString', 'expert'), ('length', (250, 270)), ('njs', 16)], True),
+        ([('longString', 'expert'), ('length', (250, 270)), ('njs', (12, 13))], False),
+        ([('longString', 'tests'), ('length', (250, 270)), ('njs', 16)], False),
+        ([('rankedState', 'Graveyard'), ('longString', 'vocal'), ('length', (250, 270)), ('njs', 16), ('mods', ['chroma'])], True),
+    ])
+def test_decoratorChaining(inputDataList, expected, mockBaseNode):
+    decoratorsDict = {
+        'longString': filterMapCacheDecorators.CheckLongString,
+        'mods': filterMapCacheDecorators.CheckValueSet
+    }
+
+    for criteriaTuple in inputDataList:
+        key, criteria = criteriaTuple
+        decorator = decoratorsDict.get(key, filterMapCacheDecorators.CheckRangeOrString)
+        mockBaseNode = decorator(mockBaseNode, key, criteria)
+    
+    assert mockBaseNode.checkCriteria() == expected
+
+        
